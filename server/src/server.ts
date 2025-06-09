9fc46ac34fc99f091d4058350fe892b53afaaab1
Kiki <kirstinlisnoff5@gmail.com>
2025-06-09 15:46:32 -0400

    Bug fixes

--- server/src/server.ts
+++ server/src/server.ts
@@ -42,10 +42,11 @@ console.log('__dirname is:', __dirname);
     );
 
   if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
-  app.get('*', (_req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
-  });
+  app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.html'));
+});
+
     }
 app.get('/', (_req, res) => {
   res.send('Express server is running');
